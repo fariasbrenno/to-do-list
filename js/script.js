de25c46a3856661ac01b93cfe86c6bf1ce673f93
Brenno Farias <brenno.developer@gmail.com>
2022-09-25 22:10:51 -0300

    Avançando javascript

--- js/script.js
+++ js/script.js
@@ -7,7 +7,32 @@ const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 //Funções
+const saveTodo = (text) => {
 
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo)
+}
 //Eventos
 todoForm.addEventListener("submit", (e) =>{
     e.preventDefault();
@@ -16,6 +41,6 @@ todoForm.addEventListener("submit", (e) =>{
 
     if(inputValue){
         //Save todo
-        console.log(inputValue);
+        saveTodo(inputValue);
     }
 })
